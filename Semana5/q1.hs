32c4fcc6aad5e1cb799b4ea79903f0ecc8473940
Ronaldo Rodrigues <ronaldo182019@gmail.com>
2022-07-28 00:50:58 -0300

    semana 5 finalizada

--- Semana5/q1.hs
+++ Semana5/q1.hs
@@ -16,19 +16,16 @@ type Nome = String
 type CPF = String
 type Idade = Int
 
-data Pessoa = Adulto Nome CPF Idade [Pessoa] | Crianca Nome CPF Idade deriving (Show)
+data Pessoa = Adulto Nome CPF Idade [Pessoa] | Crianca Nome CPF Idade deriving (Eq, Show)
 
 
-
-
+-- sugestão para testar: ehFilho (Adulto "fulano" "123" 40 [(Crianca "um" "321" 12), (Adulto "dois" "231" 18 [])]) (Crianca "um" "321" 12)
 ehFilho :: Pessoa -> Pessoa -> Bool
-ehFilho (Adulto n c i as) p2 = if length (filter (==p2) as) > 0 then True else False
+ehFilho (Adulto _ _ _ as) p2 = length (filter (==p2) as) > 0
 
-
-
+-- sugestão para testar: mostraPessoa (Adulto "fulano" "123" 40 [(Crianca "um" "321" 12), (Adulto "dois" "231" 18 [])])
 mostraPessoa :: Pessoa -> String
-mostraPessoa a = "a"
-
+mostraPessoa (Adulto n c i as) = n ++ ", " ++ c ++ ", " ++ show i ++ ", " ++ show as
 
 
 
---
+++ Semana5/q2.hs
@@ -0,0 +1,65 @@
+import Data.Type.Bool (Not)
+{-
+Blockchain é basicamente um banco de dados distribuído onde os dados são armazenados em blocos 
+onde cada bloco possui transações financeiras e uma referência para o próximo bloco formando uma corrente. 
+Assumindo os tipos de dados abaixo, crie uma função em Haskell que dado um Bloco e uma String de uma conta 
+retornam uma tupla-2, no qual o primeiro elemento corresponde a média dos valores das transações em toda a 
+corrente nas quais a conta passada como parâmetro era a origem (de), e o segundo elemento contém também a 
+média dos valores mas quando a conta era destino (para).
+
+data Transacao = Transacao { de :: String -- Conta que paga
+
+                                                , para :: String -- Conta que recebe
+
+                                                 , valor :: Float -- Quanto está pagando
+
+                                                } deriving (Show)
+
+data Bloco = Bloco { indice :: Int -- Indice do bloco
+
+                                 , trs :: [Transacao] -- Lista de transações de um bloco
+
+                                 , proximo :: Maybe Bloco -- Proximo bloco
+
+                                 } deriving (Show)
+
+OBS: para definir um valor do tipo bloco é só especificar (Bloco ind trs prox), onde ind é um inteiro, 
+trs é uma lista de transações e prox é um Maybe Bloco. O mesmo se aplica ao tipo Transacao.
+-}
+
+
+data Transacao = Transacao { de :: String -- Conta que paga
+                             , para :: String -- Conta que recebe
+                             , valor :: Float -- Quanto está pagando
+                           } deriving (Show)
+
+data Bloco = Bloco { indice :: Int -- Indice do bloco
+                     , trs  :: [Transacao] -- Lista de transações de um bloco
+                     , proximo :: Maybe Bloco -- Proximo bloco
+                   } deriving (Show)
+
+
+-- para testar: medias (Just (Bloco 0 [(Transacao "001" "002" 150), (Transacao "001" "003" 100), (Transacao "002" "001" 180), (Transacao "003" "004" 120)] (Just (Bloco 1 [(Transacao "004" "002" 150), (Transacao "001" "003" 100), (Transacao "002" "001" 180), (Transacao "004" "001" 120)] Nothing)))) "001"
+
+juntarListas :: Maybe Bloco -> [Transacao]
+juntarListas Nothing = []
+juntarListas (Just (Bloco _ ts b)) = ts ++ juntarListas b
+
+getValorTransacao :: Transacao -> Float
+getValorTransacao (Transacao _ _ v) = v
+
+somarLista :: [Transacao] -> Float
+somarLista = foldr ((+) . getValorTransacao) 0
+
+contaDeIgual :: String -> Transacao -> Bool
+contaDeIgual c (Transacao d _ _) = c == d
+
+contaFromIgual :: String -> Transacao -> Bool
+contaFromIgual c (Transacao _ f _) = c == f
+
+medias :: Maybe Bloco -> String -> (Float, Float)
+medias b c = (somarLista (filtro (contaDeIgual c)) / fromIntegral (length (filtro (contaDeIgual c))), somarLista (filtro (contaFromIgual c)) / fromIntegral (length (filtro (contaFromIgual c))))
+            where filtro x = filter x (juntarListas b)
+
+
+
